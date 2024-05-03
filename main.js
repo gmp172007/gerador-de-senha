8d8c4814d2b0fbf3b9b08535c70bd66667161725
gmp172007 <111883418+gmp172007@users.noreply.github.com>
2024-05-03 10:51:54 -0300

    .

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const latrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
