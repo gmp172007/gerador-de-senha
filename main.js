4588f2a088594c31552fa33057729a7edc9b77ab
gmp172007 <111883418+gmp172007@users.noreply.github.com>
2024-05-03 10:48:29 -0300

    .

---
+++ main.js
@@ -0,0 +1,6 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const latrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
